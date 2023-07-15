b9bfcf4cebfc16ccb3700ebfbcc833f7065da6cf
isabel <isabel@isabelroses.com>
2023-07-15 02:39:06 +0100

    small changes

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -7,7 +7,7 @@ return {
     -- stylua: ignore
     right_mouse_command = function(n) require("mini.bufremove").delete(n, false) end,
 			diagnostics = "nvim_lsp",
-			always_show_bufferline = false,
+			always_show_bufferline = true,
 			diagnostics_indicator = function(_, _, diag)
 				local icons = require("lazyvim.config").icons.diagnostics
 				local ret = (diag.error and icons.Error .. diag.error .. " " or "")
