c0f6440641b4bc627d7f25b9b1e986e9814d083c
isabel <isabel@isabelroses.com>
2023-07-28 10:52:35 +0100

    fix(keymaps): make them work xD

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,4 +2,22 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-Keymap("n", "<leader>qq", "<cmd>qall!<cr>")
+-- local function unmap(mode, key)
+--  vim.keymap.del(mode, key, {})
+-- end
+
+local function map(mode, lhs, rhs, opts)
+  local keys = require("lazy.core.handler").handlers.keys
+  ---@cast keys LazyKeysHandler
+  -- do not create the keymap if a lazy keys handler exists
+  if not keys.active[keys.parse({ lhs, mode = mode }).id] then
+    opts = opts or {}
+    opts.silent = opts.silent ~= false
+    if opts.remap and not vim.g.vscode then
+      opts.remap = nil
+    end
+    vim.keymap.set(mode, lhs, rhs, opts)
+  end
+end
+
+map("n", "<leader>qq", "<cmd>qall!<cr>")
