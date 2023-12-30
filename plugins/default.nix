01a8736097bf818529f7f2817f23c6f5651fe7a5
isabel <isabel@isabelroses.com>
2023-12-30 12:41:48 +0000

    idk i hate copilot

--- plugins/default.nix
+++ plugins/default.nix
@@ -235,14 +235,47 @@ in rec {
   copilot-lua = {
     src = srcs.copilot-lua;
     config = {
-      panel.enabled = true;
+      panel = {
+        enabled = true;
+        auto_refresh = false;
+        keymap = {
+          jump_prev = "[[";
+          jump_next = "]]";
+          accept = "<CR>";
+          refresh = "gr";
+          open = "<M-CR>";
+        };
+        layout = {
+          position = "bottom";
+          ratio = 0.4;
+        };
+      };
       suggestion = {
         enabled = true;
-        auto_trigger = true;
+        auto_trigger = false;
         debounce = 75;
-        keymap.accept = "<CR>";
+        keymap = {
+          accept = "<M-l>";
+          accept_word = false;
+          accept_line = false;
+          next = "<M-]>";
+          prev = "<M-[>";
+          dismiss = "<C-]>";
+        };
+      };
+      filetypes = {
+        yaml = true;
+        markdown = true;
+        help = false;
+        gitcommit = false;
+        gitrebase = false;
+        hgcommit = false;
+        svn = false;
+        cvs = false;
+        "." = false;
       };
       copilot_node_command = "node";
+      server_opts_overrides = {};
     };
   };
 
