25d4f81a60e4f9074853d389bec2573e2d9f9290
isabel <isabel@isabelroses.com>
2024-01-07 20:36:26 +0000

    chore: cleanup telescope

--- plugins/telescope.lua
+++ plugins/telescope.lua
@@ -3,13 +3,13 @@ return function()
 
 	local no_preview = function(opts)
 		local defaults = require("telescope.themes").get_dropdown({
-            -- stylua: ignore
-            borderchars = {
-                { bc.horiz, bc.vert, bc.horiz, bc.vert, bc.topleft, bc.topright, bc.botright, bc.botleft },
-                prompt = { bc.horiz, bc.vert, " ", bc.vert, bc.topleft, bc.topright, bc.vert, bc.vert },
-                results = { bc.horiz, bc.vert, bc.horiz, bc.vert, bc.vertright, bc.vertleft, bc.botright, bc.botleft },
-                preview = { bc.horiz, bc.vert, bc.horiz, bc.vert, bc.topleft, bc.topright, bc.botright, bc.botleft },
-            },
+      -- stylua: ignore
+      borderchars = {
+        { bc.horiz, bc.vert, bc.horiz, bc.vert, bc.topleft, bc.topright, bc.botright, bc.botleft },
+        prompt = { bc.horiz, bc.vert, " ", bc.vert, bc.topleft, bc.topright, bc.vert, bc.vert },
+        results = { bc.horiz, bc.vert, bc.horiz, bc.vert, bc.vertright, bc.vertleft, bc.botright, bc.botleft },
+        preview = { bc.horiz, bc.vert, bc.horiz, bc.vert, bc.topleft, bc.topright, bc.botright, bc.botleft },
+      },
 			width = 0.8,
 			previewer = false,
 			prompt_title = false,
@@ -20,8 +20,6 @@ return function()
 
 	local telescope = require("telescope")
 
-	telescope.load_extension("asynctasks")
-	telescope.load_extension("file_browser")
 	telescope.load_extension("fzf")
 	telescope.load_extension("notify")
 	telescope.load_extension("project")
@@ -32,8 +30,8 @@ return function()
 			prompt_prefix = " ",
 			selection_caret = " ",
 			multi_icon = "│",
-            -- stylua: ignore
-            borderchars = { bc.horiz, bc.vert, bc.horiz, bc.vert, bc.topleft, bc.topright, bc.botright, bc.botleft },
+      -- stylua: ignore
+      borderchars = { bc.horiz, bc.vert, bc.horiz, bc.vert, bc.topleft, bc.topright, bc.botright, bc.botleft },
 		},
 		pickers = {
 			find_files = no_preview(),
@@ -68,13 +66,11 @@ return function()
 		},
 		["<leader>f"] = {
 			name = "+Telescope",
-			b = { "<cmd>Telescope file_browser grouped=true<cr>", "File browser" },
 			g = { "<cmd>Telescope live_grep<cr>", "Live grep" },
-			h = { "<cmd>Telescope help_tags<cr>", "Help tags" },
-			n = { "<cmd>Telescope notify<cr>", "Show notifications" },
-			p = { "<cmd>Telescope project<cr>", "Project" },
-			r = { "<cmd>Telescope asynctasks all<cr>", "Run asynctasks" },
 			s = { "<cmd>SessionManager load_session<cr>", "Show sessions" },
+			-- h = { "<cmd>Telescope help_tags<cr>", "Help tags" },
+			-- n = { "<cmd>Telescope notify<cr>", "Show notifications" },
+			-- p = { "<cmd>Telescope project<cr>", "Project" },
 		},
 	})
 end
