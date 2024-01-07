cff88c5267bdb4704450cdd3da20249637c3b615
isabel <isabel@isabelroses.com>
2024-01-07 20:33:23 +0000

    feat: trouble

---
+++ plugins/trouble.lua
@@ -0,0 +1,16 @@
+return function()
+	local trouble = require("trouble")
+	trouble.setup({})
+
+	vim.keymap.set("n", "<leader>tt", function()
+		trouble.toggle()
+	end, { noremap = true, silent = true })
+
+	vim.keymap.set("n", "[d", function()
+		trouble.next({ skip_groups = true, jump = true })
+	end, { noremap = true, silent = true })
+
+	vim.keymap.set("n", "]d", function()
+		trouble.previous({ skip_groups = true, jump = true })
+	end, { noremap = true, silent = true })
+end
