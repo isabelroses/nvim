6fcea08a30008de6ff3ad6119676d6afc55eada1
isabel <isabel@isabelroses.com>
2024-01-07 20:35:56 +0000

    chore(harpoon): no more telescope

--- plugins/harpoon.lua
+++ plugins/harpoon.lua
@@ -2,28 +2,9 @@ return function()
 	local harpoon = require("harpoon")
 	harpoon:setup({})
 
-	local conf = require("telescope.config").values
-	local function toggle_telescope(harpoon_files)
-		local file_paths = {}
-		for _, item in ipairs(harpoon_files.items) do
-			table.insert(file_paths, item.value)
-		end
-
-		require("telescope.pickers")
-			.new({}, {
-				prompt_title = "Harpoon",
-				finder = require("telescope.finders").new_table({
-					results = file_paths,
-				}),
-				previewer = conf.file_previewer({}),
-				sorter = conf.generic_sorter({}),
-			})
-			:find()
-	end
-
 	vim.keymap.set("n", "<leader>d", function()
-		toggle_telescope(harpoon:list())
-	end, { desc = "Open harpoon window" })
+		harpoon.ui:toggle_quick_menu(harpoon:list())
+	end)
 
 	vim.keymap.set("n", "<leader>a", function()
 		harpoon:list():append()
