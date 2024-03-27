e8b17b3c1a4e669408ab594592040911f596dba3
isabel <isabel@isabelroses.com>
2024-03-27 20:15:06 +0000

    chore: update charm-freeze conf

--- plugins/charm-freeze.lua
+++ plugins/charm-freeze.lua
@@ -5,5 +5,12 @@ return function()
 		["<leader>sc"] = { ":Freeze<cr>", "Snapshot code" },
 	}, { mode = "v" })
 
-	require("charm-freeze").setup()
+	require("charm-freeze").setup({
+		output = function()
+			return "./" .. os.date("%Y-%m-%d") .. "_freeze.png"
+		end,
+		theme = "catppuccin-mocha",
+		window = true,
+		padding = { 20, 40, 20, 20 },
+	})
 end
