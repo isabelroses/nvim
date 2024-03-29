ca7358e6a9e9bbc1535077ec26f537816210ca29
isabel <isabel@isabelroses.com>
2024-03-29 10:31:49 +0000

    style: more formating

--- helpers/lazy2nvfetcher.lua
+++ helpers/lazy2nvfetcher.lua
@@ -2,40 +2,40 @@ local plugins = require("lazy").plugins()
 
 -- drop the trailing `.nvim`, replace dots with dashes
 local function toNvfetcherName(name)
-	return name:lower():gsub("%.nvim$", ""):gsub("%.", "-")
+  return name:lower():gsub("%.nvim$", ""):gsub("%.", "-")
 end
 
 table.sort(plugins, function(a, b)
-	return toNvfetcherName(a.name) < toNvfetcherName(b.name)
+  return toNvfetcherName(a.name) < toNvfetcherName(b.name)
 end)
 
 local c = ""
 for _, plugin in ipairs(plugins) do
-	local name = toNvfetcherName(plugin.name)
+  local name = toNvfetcherName(plugin.name)
 
-	if plugin.url then
-		-- get rid of the .git at the end
-		local url = plugin.url:sub(1, -5)
+  if plugin.url then
+    -- get rid of the .git at the end
+    local url = plugin.url:sub(1, -5)
 
-		c = c .. "[" .. name .. "]\n"
-		if url:find("github") then
-			c = c .. 'fetch.github = "' .. url:gsub("%w+://github.com/", "") .. '"\n'
-		else
-			c = c .. 'fetch.git = "' .. url .. '"\n'
-		end
-		c = c .. 'src.git = "' .. url .. '"\n'
-	end
+    c = c .. "[" .. name .. "]\n"
+    if url:find("github") then
+      c = c .. 'fetch.github = "' .. url:gsub("%w+://github.com/", "") .. '"\n'
+    else
+      c = c .. 'fetch.git = "' .. url .. '"\n'
+    end
+    c = c .. 'src.git = "' .. url .. '"\n'
+  end
 
-	if plugin.branch then
-		c = c .. 'src.branch = "' .. plugin.branch .. '"\n'
-	end
-	c = c .. "\n"
+  if plugin.branch then
+    c = c .. 'src.branch = "' .. plugin.branch .. '"\n'
+  end
+  c = c .. "\n"
 end
 
 -- write the plugins to a file
 local file = io.open("nvfetcher.toml", "w")
 if not file then
-	error("Could not open file for writing")
+  error("Could not open file for writing")
 end
 file:write(c)
 file:close()
--- helpers/treesitter2nvfetcher.lua
+++ helpers/treesitter2nvfetcher.lua
@@ -5,28 +5,28 @@ table.sort(parser_list)
 local c = ""
 
 for _, lang in pairs(parser_list) do
-	-- optional: check if the parser is installed
-	-- local is_installed = #vim.api.nvim_get_runtime_file("parser/" .. lang .. ".so", false) > 0
-	-- if is_installed then ... end
+  -- optional: check if the parser is installed
+  -- local is_installed = #vim.api.nvim_get_runtime_file("parser/" .. lang .. ".so", false) > 0
+  -- if is_installed then ... end
 
-	local info = parsers.get_parser_configs()[lang].install_info
+  local info = parsers.get_parser_configs()[lang].install_info
 
-	c = c .. "[tree-sitter-grammar-" .. lang .. "]\n"
-	c = c .. 'fetch.git = "' .. info.url .. '"\n'
-	c = c .. 'src.git = "' .. info.url .. '"\n'
-	if info.branch then
-		c = c .. 'src.branch = "' .. info.branch .. '"\n'
-	end
-	if info.location then
-		c = c .. 'passthru = { location = "' .. info.location .. '" }\n'
-	end
-	c = c .. "\n"
+  c = c .. "[tree-sitter-grammar-" .. lang .. "]\n"
+  c = c .. 'fetch.git = "' .. info.url .. '"\n'
+  c = c .. 'src.git = "' .. info.url .. '"\n'
+  if info.branch then
+    c = c .. 'src.branch = "' .. info.branch .. '"\n'
+  end
+  if info.location then
+    c = c .. 'passthru = { location = "' .. info.location .. '" }\n'
+  end
+  c = c .. "\n"
 end
 
 -- write the plugins to a file
 local file = io.open("pkgs/nvim-treesitter/nvfetcher.toml", "w")
 if not file then
-	error("Could not open file for writing")
+  error("Could not open file for writing")
 end
 file:write(c)
 file:close()
