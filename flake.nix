d00c7e43b670625abacc91076ff9c74eaed591eb
isabel <isabel@isabelroses.com>
2024-08-04 14:46:58 +0100

    stuff

--- flake.nix
+++ flake.nix
@@ -3,18 +3,6 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-parts = {
-      url = "github:hercules-ci/flake-parts";
-      inputs.nixpkgs-lib.follows = "nixpkgs";
-    };
-
-    git-hooks = {
-      url = "github:cachix/git-hooks.nix";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-        flake-compat.follows = "";
-      };
-    };
 
     nil = {
       url = "github:oxalica/nil";
@@ -43,15 +31,6 @@
     #   };
     # };
 
-    neovim-nix = {
-      url = "github:isabelroses/neovim.nix/dev";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-        flake-parts.follows = "flake-parts";
-        git-hooks.follows = "git-hooks";
-      };
-    };
-
     systems.url = "github:nix-systems/default";
 
     # maintenance
@@ -63,81 +42,54 @@
 
   outputs =
     inputs:
-    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+    let
       systems = import inputs.systems;
 
-      imports = [
-        inputs.neovim-nix.flakeModule
-        ./neovim.nix
-      ] ++ inputs.nixpkgs.lib.optional (inputs.git-hooks ? flakeModule) inputs.git-hooks.flakeModule;
+      inherit (inputs.nixpkgs) lib;
 
-      perSystem =
-        {
-          lib,
-          pkgs,
-          self',
-          config,
-          system,
-          ...
-        }:
-        {
-          _module.args.pkgs = import inputs.nixpkgs {
-            inherit system;
-            config.allowUnfree = true;
-            overlays = [
-              inputs.nil.overlays.nil
-              inputs.beapkgs.overlays.default
-            ];
-          };
-
-          formatter = pkgs.nixfmt-rfc-style;
-
-          packages =
-            let
-              neovim = config.neovim.final;
-            in
-            {
-              inherit neovim;
-              default = neovim;
-              nvim-treesitter = pkgs.callPackage ./pkgs/nvim-treesitter { };
-            };
-
-          devShells = {
-            default = pkgs.mkShellNoCC {
-              inherit (self'.checks.pre-commit-check) shellHook;
-              buildInputs = with pkgs; [
-                self'.formatter
-                nvfetcher
+      forAllSystem =
+        function:
+        lib.genAttrs systems (
+          system:
+          function (
+            import inputs.nixpkgs {
+              inherit system;
+              config.allowUnfree = true;
+              overlays = [
+                inputs.nil.overlays.nil
+                inputs.beapkgs.overlays.default
               ];
-            };
+            }
+          )
+        );
+    in
+    {
+      formatter = forAllSystem (pkgs: pkgs.nixfmt-rfc-style);
 
-            generate-treesitter = pkgs.mkShellNoCC {
-              packages = with pkgs; [
-                nvfetcher
-                (writeShellApplication {
-                  name = "generate";
-                  runtimeInputs = [ (callPackage ./pkgs/nvim-treesitter/neovim.nix { }) ];
-
-                  text = ''
-                    nvim --headless -l ${./pkgs/nvim-treesitter/generate-nvfetcher.lua}
-                  '';
-                })
-              ];
-            };
-          };
-        }
-        // lib.optionalAttrs (inputs.git-hooks ? flakeModule) {
-          checks.pre-commit-check = inputs.git-hooks.lib.${system}.run {
-            src = ./.;
-            excludes = [ "_sources/.+" ];
-            hooks = {
-              nixfmt = {
-                enable = true;
-                package = pkgs.nixfmt-rfc-style;
-              };
-              stylua.enable = true;
-            };
-          };
+      packages = forAllSystem (pkgs: rec {
+        default = neovim;
+        neovim = pkgs.callPackage ./neovim.nix {
+          version = inputs.self.shortRev or inputs.self.dirtyShortRev or "dirty";
         };
+        nvim-treesitter = pkgs.callPackage ./pkgs/nvim-treesitter { };
+      });
+
+      devShells = forAllSystem (pkgs: {
+        default = pkgs.mkShellNoCC { packages = with pkgs; [ nvfetcher ]; };
+
+        generate-treesitter = pkgs.mkShellNoCC {
+          packages = with pkgs; [
+            nvfetcher
+            (writeShellApplication {
+              name = "generate";
+              runtimeInputs = [ (callPackage ./pkgs/nvim-treesitter/neovim.nix { }) ];
+
+              text = ''
+                nvim --headless -l ${./pkgs/nvim-treesitter/generate-nvfetcher.lua}
+              '';
+            })
+          ];
+        };
+      });
     };
 }
--- neovim.nix
+++ neovim.nix
@@ -1,84 +1,132 @@
 {
-  config.perSystem =
-    {
-      lib,
-      pkgs,
-      # inputs',
-      ...
-    }:
-    {
-      neovim = {
-        # package = inputs'.neovim-nightly-overlay.packages.default;
-        package = pkgs.neovim-unwrapped;
+  lib,
+  pkgs,
+  version,
+  ...
+}:
+let
+  isabels-nvim = pkgs.vimUtils.buildVimPlugin {
+    pname = "isabels-nvim";
+    inherit version;
+    src = ./src;
+  };
 
-        paths = with pkgs; [
-          # external deps
-          fd
-          ripgrep
+  plugins =
+    [
+      isabels-nvim
 
-          # lua
-          stylua
-          lua-language-server
-
-          # webdev
-          emmet-language-server
-          nodePackages."@astrojs/language-server"
-          nodePackages."@tailwindcss/language-server"
-          nodePackages.typescript
-          nodePackages.vscode-langservers-extracted
-          nodePackages.graphql-language-service-cli
-          nodejs-slim
-
-          # markdown / latex
-          ltex-ls
-          nodePackages.alex
-          marksman
-
-          # nix
-          nil
-          statix
-          deadnix
-          nixfmt-rfc-style
-
-          # shell
-          shfmt
-          shellcheck
-          bash-language-server
-
-          # etc
-          nodePackages.prettier
-          proselint
-          taplo # toml
-          nodePackages.yaml-language-server # yaml
-          nodePackages.dockerfile-language-server-nodejs # docker
+      ((pkgs.callPackage ./pkgs/nvim-treesitter { }).override {
+        grammars = [
+          "astro"
+          "bash"
+          "c"
+          "cpp"
+          "css"
+          "csv"
+          "diff"
+          "dockerfile"
+          "git_rebase"
+          "gitattributes"
+          "gitcommit"
+          "gitignore"
+          "go"
+          "gomod"
+          "gosum"
+          "gotmpl"
+          "graphql"
+          "haskell"
+          "html"
+          "javascript"
+          "jsdoc"
+          "json"
+          "jsonc"
+          "just"
+          "lua"
+          "make"
+          "markdown"
+          "markdown_inline"
+          "nix"
+          "nu"
+          "php"
+          "php_only"
+          "python"
+          "rust"
+          "scss"
+          "svelte"
+          "toml"
+          "tsv"
+          "tsx"
+          "typescript"
+          "vim"
+          "vue"
+          "yaml"
+          "yuck"
+          "zig"
         ];
+      })
+    ]
+    ++ builtins.removeAttrs
+      (builtins.mapAttrs (_: val: { inherit (val) src; }) (pkgs.callPackage ./_sources/generated.nix { }))
+      [
+        "override"
+        "overrideDerivation"
+      ];
 
-        build =
-          let
-            env = pkgs.buildEnv {
-              name = "neovim-host-prog";
-              paths = [ pkgs.nodePackages.neovim ] ++ [ (pkgs.python3.withPackages (ps: with ps; [ pynvim ])) ];
-            };
-          in
-          lib.mkForce {
-            before = pkgs.writeTextFile {
-              name = "before.lua";
-              text = ''
-                -- set space as leader
-                vim.g.mapleader = " "
-                vim.g.maplocalleader = " "
+  extraPackages = with pkgs; [
+    # external deps
+    fd
+    ripgrep
 
-                -- this defaults to this and we want to keep it that way
-                vim.g.node_host_prog = "${env}/bin/neovim-node-host"
-                vim.g.python3_host_prog = "${env}/bin/python"
-              '';
-            };
-          };
+    # lua
+    stylua
+    lua-language-server
 
-        lazy = {
-          settings.dev.path = "~/dev/nvim";
-          plugins = import ./plugins { inherit pkgs; };
-        };
-      };
-    };
-}
+    # webdev
+    emmet-language-server
+    nodePackages."@astrojs/language-server"
+    nodePackages."@tailwindcss/language-server"
+    nodePackages.typescript
+    nodePackages.vscode-langservers-extracted
+    nodePackages.graphql-language-service-cli
+    nodejs-slim
+
+    # markdown / latex
+    ltex-ls
+    nodePackages.alex
+    marksman
+
+    # nix
+    nil
+    statix
+    deadnix
+    nixfmt-rfc-style
+
+    # shell
+    shfmt
+    shellcheck
+    bash-language-server
+
+    # etc
+    nodePackages.prettier
+    proselint
+    taplo # toml
+    nodePackages.yaml-language-server # yaml
+    nodePackages.dockerfile-language-server-nodejs # docker
+  ];
+
+  baseConfig = pkgs.neovimUtils.makeNeovimConfig {
+    withRuby = false;
+    inherit plugins;
+  };
+
+  config = baseConfig // {
+    luaRcContent = "require('getchoo')";
+    wrapperArgs = baseConfig.wrapperArgs ++ [
+      "--suffix"
+      "PATH"
+      ":"
+      "${lib.makeBinPath extraPackages}"
+    ];
+  };
+in
+pkgs.wrapNeovimUnstable pkgs.neovim-unwrapped config
