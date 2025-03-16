e55fbf4d8eb095ab92ef89b76aaf77d4aa2ab24a
isabel <isabel@isabelroses.com>
2025-03-16 00:04:51 +0000

    fix: rustacenvim

--- config/lua/izvim/plugins/lsp.lua
+++ config/lua/izvim/plugins/lsp.lua
@@ -508,7 +508,47 @@ return {
 
   {
     "rustaceanvim",
-    ft = "rust",
+    after = function()
+      vim.g.rustaceanvim = {
+        on_attach = function(client, bufnr)
+          vim.keymap.set("n", "<leader>cR", function()
+            vim.cmd.RustLsp("codeAction")
+          end)
+          vim.keymap.set("n", "<leader>dr", function()
+            vim.cmd.RustLsp("debuggables")
+          end, { buffer = bufnr })
+        end,
+        tools = {
+          float_win_config = {
+            border = "rounded",
+          },
+        },
+        settings = {
+          -- rust-analyzer language server configuration
+          ["rust-analyzer"] = {
+            cargo = {
+              allFeatures = true,
+              loadOutDirsFromCheck = true,
+              runBuildScripts = true,
+            },
+            -- Add clippy lints for Rust.
+            checkOnSave = {
+              allFeatures = true,
+              command = "clippy",
+              extraArgs = { "--no-deps" },
+            },
+            procMacro = {
+              enable = true,
+              ignored = {
+                ["async-trait"] = { "async_trait" },
+                ["napi-derive"] = { "napi" },
+                ["async-recursion"] = { "async_recursion" },
+              },
+            },
+          },
+        },
+      }
+    end,
   },
 
   {
