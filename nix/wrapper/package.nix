9659c3d3cfb9d15dd526953a3ed8761af0a8d693
isabel <isabel@isabelroses.com>
2025-04-06 13:05:16 +0100

    refactor: use stdenvNoCC and use init.lua

--- nix/wrapper/package.nix
+++ nix/wrapper/package.nix
@@ -3,9 +3,11 @@
 
   # builders
   runCommand,
-  runCommandLocal,
+  stdenvNoCC,
   makeBinaryWrapper,
   writeText,
+  envsubst,
+  lndir,
 
   # neovim
   pname ? "neovim",
@@ -30,64 +32,79 @@
 }:
 let
   inherit (lib.meta) getExe;
-  inherit (lib.strings) concatMapStringsSep makeBinPath escapeShellArgs;
-  inherit (builtins) typeOf;
+  inherit (lib.strings) concatMapStringsSep makeBinPath getVersion;
+  inherit (builtins) typeOf baseNameOf;
 
-  packDir = runCommandLocal "packdir" { } ''
+  inherit (lua.pkgs) luaLib;
+
+  init = writeText "init.lua" ''
+    package.path = "${luaLib.genLuaPathAbsStr luaEnv};$LUA_PATH" .. package.path
+    package.cpath = "${luaLib.genLuaCPathAbsStr luaEnv};$LUA_CPATH" .. package.cpath
+    vim.env.PATH =  vim.env.PATH .. ":${makeBinPath extraPackages}"
+    vim.opt.packpath:append('$out')
+    vim.opt.runtimepath:append('$out')
+
+    vim.loader.enable()
+    vim.g.loaded_node_provider = 0
+    vim.g.loaded_perl_provider = 0
+    vim.g.loaded_python_provider = 0
+    vim.g.loaded_python3_provider = 0
+    vim.g.loaded_ruby_provider = 0
+  '';
+
+  finalConfig = runCommand "final-config" { } ''
     mkdir -pv $out/pack/${pname}/{start,opt}
 
-     ${concatMapStringsSep "\n" (p: ''
-       ln -vsfT ${p} $out/pack/${pname}/${if (p.passthru.start or false) then "start" else "opt"}/${
-         if typeOf p == "path" then baseNameOf p else (p.pname or p.name)
-       }
-     '') plugins}
+    ${concatMapStringsSep "\n" (p: ''
+      ln -vsfT ${p} $out/pack/${pname}/${if (p.passthru.start or false) then "start" else "opt"}/${
+        if typeOf p == "path" then baseNameOf p else (p.pname or p.name)
+      }
+    '') plugins}
 
     ln -vsfT ${userConfig} $out/pack/${pname}/start/init-plugin
-  '';
 
-  rc = writeText "rc.vim" ''
-    lua package.path = "${lua.pkgs.luaLib.genLuaPathAbsStr luaEnv}"; package.cpath = "${lua.pkgs.luaLib.genLuaCPathAbsStr luaEnv}"
-    set packpath^=${packDir} | set runtimepath^=${packDir}
-
-    lua vim.loader.enable()
-    lua vim.g.loaded_node_provider = 0
-    lua vim.g.loaded_perl_provider = 0
-    lua vim.g.loaded_python_provider = 0
-    lua vim.g.loaded_python3_provider = 0
-    lua vim.g.loaded_ruby_provider = 0
+    ${getExe envsubst} < '${init}' > "$out/init.lua"
   '';
 in
-runCommand pname
-  {
-    inherit pname;
+stdenvNoCC.mkDerivation {
+  inherit pname;
+  version = getVersion basePackage;
 
-    __structuredAttrs = true;
+  __structuredAttrs = true;
 
-    nativeBuildInputs = [ makeBinaryWrapper ];
+  nativeBuildInputs = [
+    makeBinaryWrapper
+    lndir
+  ];
 
-    wrapperArgs = [
-      "--suffix"
-      "PATH"
-      ":"
-      (makeBinPath extraPackages)
+  dontUnpack = true;
+  strictDeps = true;
+  dontRewriteSymlinks = true;
 
-      "--add-flags"
-      (escapeShellArgs [
-        "-u"
-        (toString rc)
-      ])
+  wrapperArgs = [
+    "--add-flags"
+    "-u ${finalConfig}/init.lua"
 
-      "--set"
-      "NVIM_APPNAME"
-      pname
-    ];
+    "--set-default"
+    "NVIM_APPNAME"
+    pname
+  ];
 
-    meta = {
-      inherit (basePackage.meta) description mainProgram;
-    };
-  }
-  ''
-    makeWrapper ${getExe basePackage} $out/bin/nvim "''${wrapperArgs[@]}"
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out
+    lndir -silent ${basePackage} $out
+    rm -rf $out/share/applications
+
+    wrapProgram $out/bin/nvim "''${wrapperArgs[@]}"
 
     ln -s $out/bin/nvim $out/bin/${pname}
-  ''
+
+    runHook postInstall
+  '';
+
+  meta = basePackage.meta // {
+    priority = (basePackage.meta.priority or 0) - 2;
+  };
+}
