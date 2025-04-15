ee3e4f535911517ca4688d16956bc766fa7c65e8
isabel <isabel@isabelroses.com>
2025-04-15 22:12:47 +0100

    refactor: themes are now one file

--- config/lua/izvim/plugins/themes.lua
+++ config/lua/izvim/plugins/themes.lua
@@ -1,5 +1,41 @@
 return {
   {
+    "evergarden",
+    after = function()
+      require("evergarden").setup({
+        theme = {
+          variant = "winter",
+          accent = "pink",
+        },
+        editor = {
+          transparent_background = false,
+          override_terminal = true,
+        },
+        style = {
+          tabline = { "reverse" },
+          search = { "reverse" },
+          incsearch = { "reverse" },
+          types = { "italic" },
+          keyword = {},
+          comment = { "italic" },
+        },
+        overrides = {}, -- add custom overrides
+      })
+    end,
+  },
+
+  {
+    "cuddlefish.nvim",
+    after = function()
+      require("cuddlefish").setup({
+        overrides = {},
+      })
+
+      vim.cmd.colorscheme("cuddlefish")
+    end
+  },
+
+  {
     "catppuccin",
     priority = 1000,
     after = function()
