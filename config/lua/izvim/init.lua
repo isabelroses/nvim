f0fd94d17cc581b1a346b3b27ce7f470ba348632
robin <comfysage@isabelroses.com>
2025-06-10 09:35:22 +0100

    refactor(lsp): move to separate module

--- config/lua/izvim/init.lua
+++ config/lua/izvim/init.lua
@@ -6,6 +6,14 @@ require("izvim.config.keybinds")
 require("izvim.config.neovide")
 require("izvim.config.options")
 
+vim.api.nvim_create_autocmd({ "BufReadPre", "BufNewFile" }, {
+  group = vim.api.nvim_create_augroup('lsp:setup', { clear = true }),
+  callback = function()
+    require('izvim.config.lsp')
+  end,
+  once = true,
+})
+
 require("lz.n").load("izvim.plugins")
 
 require("izvim.health").loaded = true
