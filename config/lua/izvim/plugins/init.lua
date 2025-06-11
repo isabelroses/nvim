3dc4c921bf6b51d57de82594cea9c0ced9dad0f6
isabel <isabel@isabelroses.com>
2025-06-11 18:51:32 +0100

    chore: remove alpha

--- config/lua/izvim/plugins/init.lua
+++ config/lua/izvim/plugins/init.lua
@@ -88,4 +88,35 @@ return {
       require("gitsigns").setup()
     end,
   },
+
+  {
+    "resession.nvim",
+    lazy = false,
+    after = function()
+      local resession = require("resession")
+      resession.setup()
+
+      vim.api.nvim_create_autocmd("VimEnter", {
+        callback = function()
+          -- Only load the session if nvim was started with no args and without reading from stdin
+          if vim.fn.argc(-1) == 0 and not vim.g.using_stdin then
+            -- Save these to a different directory, so our manual sessions don't get polluted
+            resession.load(vim.fn.getcwd(), { silence_errors = true })
+          end
+        end,
+        nested = true,
+      })
+      vim.api.nvim_create_autocmd("VimLeavePre", {
+        callback = function()
+          resession.save(vim.fn.getcwd(), { notify = false })
+        end,
+      })
+      vim.api.nvim_create_autocmd("StdinReadPre", {
+        callback = function()
+          -- Store this for later
+          vim.g.using_stdin = true
+        end,
+      })
+    end,
+  },
 }
